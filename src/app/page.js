0bc272b4ec2787a962d102dffd5594f35cec22c9
yongbinkwon <92711444+BinCrow@users.noreply.github.com>
2023-09-07 19:21:12 +0900

    컴포넌트 완료

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,35 @@
+"use client";
+import React, { useEffect } from "react";
+import Header from "@/components/Header";
+import Skip from "@/components/Skip";
+import Intro from "@/components/intro";
+import Skill from "@/components/skill";
+import Site from "@/components/Site";
+import Port from "@/components/port";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import lenis from "@/utils/lenis";
+import link from "@/utils/link";
+
+
 export default function Home(){
+  useEffect(()=>{
+    lenis();
+    link();
+  }, []);
+  
   return (
-    <div>home</div>
+    <>
+      <Skip />
+      <Header />
+      <main id="main" role="main">
+        <Intro />
+        <Skill />
+        <Site />
+        <Port />
+        <Contact />
+      </main>
+      <Footer />
+    </>
   )
 }
